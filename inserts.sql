f41a1135373d64bc223d8a5eea2067c98908039a
damagedcodaa <averoly@mail.ru>
2017-05-13 14:34:55 +0300

    sql inserts script added

---
+++ inserts.sql
@@ -0,0 +1,27 @@
+-- insert into skills
+insert into skills (skill_name)
+values ('Skill_1');
+
+insert into skills (skill_name)
+values ('Skill_2');
+
+insert into skills (skill_name)
+values ('Skill_3');
+
+insert into skills (skill_name)
+values ('Skill_4');
+
+-- insert into serving_areas
+insert into serving_areas (serving_area_name)
+values ('Serving_Area_1');
+
+insert into serving_areas (serving_area_name)
+values ('Serving_Area_2');
+
+insert into serving_areas (serving_area_name)
+values ('Serving_Area_3');
+
+insert into serving_areas (serving_area_name)
+values ('Serving_Area_4');
+
+-- insert into positions
